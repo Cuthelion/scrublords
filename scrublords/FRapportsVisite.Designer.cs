af53c1525c3844f854ed152ec750f7e2957a2280
Robin Flenet <robin.flenet@outlook.com>
2019-09-25 16:46:32 +0200

    test push robin

--- scrublords/FRapportsVisite.Designer.cs
+++ scrublords/FRapportsVisite.Designer.cs
@@ -29,11 +29,37 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dgvRapVis = new System.Windows.Forms.DataGridView();
+            this.bsRapVis = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRapVis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsRapVis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRapVis
+            // 
+            this.dgvRapVis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRapVis.Location = new System.Drawing.Point(276, 88);
+            this.dgvRapVis.Name = "dgvRapVis";
+            this.dgvRapVis.Size = new System.Drawing.Size(240, 150);
+            this.dgvRapVis.TabIndex = 0;
+            // 
+            // FRapportsVisite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvRapVis);
+            this.Name = "FRapportsVisite";
             this.Text = "FRapportsVisite";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRapVis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsRapVis)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvRapVis;
+        private System.Windows.Forms.BindingSource bsRapVis;
     }
 }
--- scrublords/FRapportsVisite.cs
+++ scrublords/FRapportsVisite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,8 @@ namespace scrublords
         public FRapportsVisite()
         {
             InitializeComponent();
-        }
+            bsRapVis.DataSource = Modele.listeRapVis();
+            dgvRapVis.DataSource = bsRapVis.DataSource;
+            dgvRapVis.Colums[0].Visible = false;
     }
 }
